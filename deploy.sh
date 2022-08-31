21ff176bfe7b3fd58044f324b63ebcc91bb8e2b9
Pack krittaphat <krittaphatkmitl@gmail.com>
2022-08-31 15:31:10 +0700

    fix: filename

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ gcloud run deploy workflow-poc --source . --ingress internal --allow-unauthentic
 
 WORKFLOW_NAME=workflow
 WORKFLOW_NAME=workflow-sleep
-WORKFLOW_NAME=workflow-pararell
-WORKFLOW_NAME=workflow-pararell-check
+WORKFLOW_NAME=workflow-parallel
+WORKFLOW_NAME=workflow-parallel-check
 
-WORKFLOW_NAME=workflow
+WORKFLOW_NAME=workflow-parallel
 SERVICE_ACCOUNT=workflow@poc-gcp-359507.iam.gserviceaccount.com
 gcloud workflows deploy $WORKFLOW_NAME --source $WORKFLOW_NAME.yml --service-account $SERVICE_ACCOUNT
